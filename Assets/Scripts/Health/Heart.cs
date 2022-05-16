bb646482b161a050312d1ae3d83499e00d5104ce
userUnity <unitvladimir@gmail.com>
2022-05-16 11:15:38 +1000

    Final

---
+++ Assets/Scripts/Health/Heart.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class Heart : MonoBehaviour
+{
+    [SerializeField] private float _lerpDuration;
+
+    private Image _image;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+        _image.fillAmount = 1;
+    }
+
+    public void ToFill()
+    {
+        StartCoroutine(Filing(0, 1, _lerpDuration, Fill));
+    }
+
+    public void ToEmpty()
+    {
+        StartCoroutine(Filing(1, 0, _lerpDuration, Destroy));
+    }
+
+    private void Destroy(float value)
+    {
+        _image.fillAmount = value;
+        Destroy(gameObject);
+    }
+
+    private void Fill(float value)
+    {
+        _image.fillAmount = value;
+    }
+    
+    private IEnumerator Filing(float startValue, float endValue, float duration, UnityAction<float> lerpingEnd)
+    {
+        float elased = 0;
+        float nexValue;
+
+        while (elased < duration)
+        {
+            nexValue = Mathf.Lerp(startValue, endValue, elased / duration);
+            _image.fillAmount = nexValue;
+            elased += Time.deltaTime;
+            yield return null;
+        }
+        lerpingEnd?.Invoke(endValue);
+    }
+}
